7897b3ae4797c1b21da87ce8f98c96556347f99b
Marlies-boop <mkuipers28@st.noorderpoort.nl>
2023-12-01 10:56:03 +0100

    first push

---
+++ AdventCalender2023/AdventCalender2023/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+namespace AdventCalender2023;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
