058f58c1c60342b6f9b997abc9205c5f535d8191
Marlies-boop <113506352+Marlies-boop@users.noreply.github.com>
2023-12-04 07:28:50 +0100

    december 2 update

--- AdventCalender2023/AdventCalender2023/December_2.cs
+++ AdventCalender2023/AdventCalender2023/December_2.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventCalender2023
 {
     internal class December_2
     {
+        /*public static string fileName = "D2.txt";
+        public StreamReader reader = File.OpenText(fileName);
+
+        public void Part1()
+        {
+            //12 red, 13 green, 14 blue
+            const int maxRed = 12, maxGreen = 13, maxBlue = 14;
+            var games = Input.ToLines().Select(x => ParseGame(x)).ToList();
+
+            
+        }
+        */
+
     }
 }
